7cf1067d135304d93c3777a4a27b0560ee5152d0
FarhanShoukat <farhanshoukat0412@gmail.com>
2018-10-23 08:33:10 +0500

    updated

--- docLengthCalculator.py
+++ docLengthCalculator.py
@@ -4,7 +4,7 @@ with open('doc_index.txt') as f:
 doc_len = {}
 for line in content:
     line = line.split('\t')
-    doc_len[line[0]] = doc_len.get(line[0], 0) + len(line) -2
+    doc_len[line[0]] = doc_len.get(line[0], 0) + len(line) - 2
 
 to_write = '\n'.join([doc + '\t' + str(length) for doc, length in doc_len.items()])
 with open('doc_lengths.txt', mode='w') as f:
